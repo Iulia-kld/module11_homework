a5fdb817af3dc41ddbfcc26819cbf0f7886c7de3
Julia <118815057+Iulia-kld@users.noreply.github.com>
2023-03-07 15:32:37 +0200

    Add files via upload

--- scr/func.test.js
+++ scr/func.test.js
@@ -8,3 +8,14 @@ describe('The word and the number of its repetitions', () => {
 	expect(repeatWord('самолёт', 3)).toBe('самолёт1, самолёт2, самолёт3, ');
 	});
 }); 
+
+//продумать тест-кейсы (успешное/неуспешное выполнение функции)
+test ('The word and the number of its repetitions', () => {
+	expect(repeatWord('мир', 4)).toBe('мир1, мир2, мир3, мир4, ');
+}); 
+
+
+test ('The word and the number of its repetitions', () => {
+	expect(repeatWord('мир', 4)).toBe('мир1, мир2, мир3, ');
+}); 
+
